512118862e01a5bf145e79973a4ae106fd45cb3b
George Fandridis <fandridis@gmail.com>
2018-10-20 11:27:59 +0200

    Add tests

---
+++ src/components/LoadingIndicator/__tests__/index.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import LoadingIndicator from '../index';
+
+describe('<LoadingIndicator />', () => {
+  it('should render an <div> tag', () => {
+    const renderedComponent = shallow(<LoadingIndicator />);
+    expect(renderedComponent.type()).toEqual('div');
+  });
+
+  it('should have a className attribute', () => {
+    const renderedComponent = shallow(<LoadingIndicator />);
+    expect(renderedComponent.prop('className')).toBeDefined();
+  });
+
+  it('should adopt a valid attribute', () => {
+    const id = 'test';
+    const renderedComponent = shallow(<LoadingIndicator id={id} />);
+    expect(renderedComponent.prop('id')).toEqual(id);
+  });
+
+  it('should not adopt an invalid attribute', () => {
+    const renderedComponent = shallow(<LoadingIndicator attribute="test" />);
+    expect(renderedComponent.prop('attribute')).toBeUndefined();
+  });
+});
